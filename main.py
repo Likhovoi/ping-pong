76be4a4d0f41da4a4df2f1ecf366a327c97cb8f1
Likhovoi <96009252+Likhovoi@users.noreply.github.com>
2022-05-08 09:14:26 +0200

    Added score and scoreboard

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import random
 
 init()
 wind = display.set_mode((wind_width, wind_height))
+
+
 game = True
+score1 = 0
+score2 = 0
 
 
 class GameSprite(sprite.Sprite):
@@ -44,34 +48,27 @@ class Ball(GameSprite):
 
     def update(self, player1: Player, player2: Player):
         global game
+        global score1
+        global score2
         self.draw()
         if self.rect.y >= 497 or self.rect.y <= 3:
             self.y_speed *= -1
-        if self.rect.x <= 100:
-            if self.rect.colliderect(player1.rect):
-                self.x_speed *= -1
-            else:
-                wind.blit(main_font.render('Player 1 loose', False, loose_label_color), message_area)
-                display.update()
-                fps_timer.tick(1/game_over_time)
-                pygame.quit()
-                sys.exit()
+        if self.rect.x <= player1x:
+            if not self.rect.colliderect(player1.rect):
+                score2 += 1
+            self.x_speed *= -1
+
         if self.rect.x >= 600:
-            if self.rect.colliderect(player2.rect):
-                self.x_speed *= -1
-            else:
-                wind.blit(main_font.render('Player 2 loose', False, loose_label_color), message_area)
-                display.update()
-                fps_timer.tick(1/game_over_time)
-                pygame.quit()
-                sys.exit()
+            if not self.rect.colliderect(player2.rect):
+                score1 += 1
+            self.x_speed *= -1
         self.rect.x += self.x_speed
         self.rect.y += self.y_speed
 
 
 fps_timer = time.Clock()
-player1 = Player(80, [K_w, K_s])
-player2 = Player(600, [K_UP, K_DOWN])
+player1 = Player(player1x, [K_w, K_s])
+player2 = Player(player2x, [K_UP, K_DOWN])
 ball = Ball()
 main_font = font.SysFont('Arial', 20)
 message_area = Rect(message_area_coo)
@@ -83,6 +80,7 @@ def game_loop():
         display.update()
 
         draw.rect(wind, (255, 255, 255), Rect(0, 0, wind_width, wind_height))
+        wind.blit(main_font.render(f'Player1: {score1}   Player2: {score2}', False, scoreboard_color), Rect(scoreboard_coo))
         player1.update()
         player2.update()
         ball.update(player1, player2)
@@ -90,6 +88,20 @@ def game_loop():
         for eve in event.get():
             if eve.type == QUIT:
                 game = False
+
+        if score1 >= 10:
+            wind.blit(main_font.render('Player 1 win', False, loose_label_color), message_area)
+            display.update()
+            fps_timer.tick(1 / game_over_time)
+            pygame.quit()
+            sys.exit()
+
+        if score2 >= 10:
+            wind.blit(main_font.render('Player 2 win', False, loose_label_color), message_area)
+            display.update()
+            fps_timer.tick(1 / game_over_time)
+            pygame.quit()
+            sys.exit()
 
         fps_timer.tick(fps)
 
--- settings.py
+++ settings.py
@@ -1,13 +1,24 @@
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+
 wind_width = 700
 wind_height = 500
 fps = 60
 game_over_time = 3
 message_area_coo = [340, 200, 100, 20]
 
+max_score = 10
+scoreboard_coo = [300, 0, 100, 20]
+scoreboard_color = GREEN
+
 player_speed = 5
 player_y = 200
 player_size = [20, 100]
+player1x = 80
+player2x = 600
 
 ball_speed = 3
 ball_size = [50, 50]
-loose_label_color = (255, 0, 0)
+loose_label_color = RED
